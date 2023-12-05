733ca6dfe45707a147da63a3b28f4ced31aca27b
Afifah Salimi <afifahsalimi@gmail.com>
2023-12-05 22:08:21 +0800

    added move horizontally with joystick

--- lib/components/player.dart
+++ lib/components/player.dart
@@ -2,11 +2,12 @@ import 'dart:async';
 
 import 'package:flame/components.dart';
 import 'package:flame_2d_game/pixel_adventure.dart';
+import 'package:flutter/services.dart';
 
 enum PlayerState { idle, running }
 
 class Player extends SpriteAnimationGroupComponent
-    with HasGameRef<PixelAdventure> {
+    with HasGameRef<PixelAdventure>, KeyboardHandler {
   String character;
   Player({
     super.position,
@@ -17,8 +18,51 @@ class Player extends SpriteAnimationGroupComponent
   late final SpriteAnimation idleAnimation;
   late final SpriteAnimation runningAnimation;
 
+  double horizontalMovement = 0;
+  Vector2 startingPosition = Vector2.zero();
+  Vector2 velocity = Vector2.zero();
+  double moveSpeed = 100;
+  double fixedDeltaTime = 1 / 60;
+  double accumulatedTime = 0;
+
   @override
   FutureOr<void> onLoad() {
+    _loadAllAnimations();
+
+    startingPosition = Vector2(position.x, position.y);
+
+    return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    accumulatedTime += dt;
+
+    while (accumulatedTime >= fixedDeltaTime) {
+      _updatePlayerState();
+      _updatePlayerMovement(fixedDeltaTime);
+
+      accumulatedTime -= fixedDeltaTime;
+    }
+
+    super.update(dt);
+  }
+
+  @override
+  bool onKeyEvent(RawKeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    horizontalMovement = 0;
+    final isLeftKeyPressed = keysPressed.contains(LogicalKeyboardKey.keyA) ||
+        keysPressed.contains(LogicalKeyboardKey.arrowLeft);
+    final isRightKeyPressed = keysPressed.contains(LogicalKeyboardKey.keyD) ||
+        keysPressed.contains(LogicalKeyboardKey.arrowRight);
+
+    horizontalMovement += isLeftKeyPressed ? -1 : 0;
+    horizontalMovement += isRightKeyPressed ? 1 : 0;
+
+    return super.onKeyEvent(event, keysPressed);
+  }
+
+  void _loadAllAnimations() {
     idleAnimation = _spriteAnimation('Idle', 11);
     runningAnimation = _spriteAnimation('Run', 12);
 
@@ -28,8 +72,6 @@ class Player extends SpriteAnimationGroupComponent
     };
 
     current = PlayerState.idle;
-
-    return super.onLoad();
   }
 
   SpriteAnimation _spriteAnimation(String state, int amount) {
@@ -41,5 +83,25 @@ class Player extends SpriteAnimationGroupComponent
         textureSize: Vector2.all(32),
       ),
     );
+  }
+
+  void _updatePlayerState() {
+    PlayerState playerState = PlayerState.idle;
+
+    if (velocity.x < 0 && scale.x > 0) {
+      flipHorizontallyAroundCenter();
+    } else if (velocity.x > 0 && scale.x < 0) {
+      flipHorizontallyAroundCenter();
+    }
+
+    // Check if moving, set running
+    if (velocity.x > 0 || velocity.x < 0) playerState = PlayerState.running;
+
+    current = playerState;
+  }
+
+  void _updatePlayerMovement(double dt) {
+    velocity.x = horizontalMovement * moveSpeed;
+    position.x += velocity.x * dt;
   }
 }
--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -2,14 +2,20 @@ import 'dart:async';
 import 'dart:ui';
 
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flame_2d_game/components/level.dart';
 import 'package:flame_2d_game/components/player.dart';
+import 'package:flutter/painting.dart';
 
-class PixelAdventure extends FlameGame {
+class PixelAdventure extends FlameGame
+    with HasKeyboardHandlerComponents, DragCallbacks {
   late CameraComponent cam;
   Player player = Player(character: 'Mask Dude');
   List<String> levelNames = ['Level-01', 'Level-02'];
+  late JoystickComponent joystick;
+  bool showControls = true;
+
   int currentLevelIndex = 0;
 
   @override
@@ -21,7 +27,55 @@ class PixelAdventure extends FlameGame {
 
     _loadLevel();
 
+    if (showControls) {
+      addJoystick();
+    }
+
     return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    if (showControls) {
+      updateJoystick();
+    }
+    super.update(dt);
+  }
+
+  void addJoystick() {
+    joystick = JoystickComponent(
+      priority: 10,
+      knob: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('HUD/Knob.png'),
+        ),
+      ),
+      background: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('HUD/Joystick.png'),
+        ),
+      ),
+      margin: const EdgeInsets.only(left: 32, bottom: 32),
+    );
+
+    add(joystick);
+  }
+
+  void updateJoystick() {
+    switch (joystick.direction) {
+      case JoystickDirection.left:
+      case JoystickDirection.upLeft:
+      case JoystickDirection.downLeft:
+        player.horizontalMovement = -1;
+        break;
+      case JoystickDirection.right:
+      case JoystickDirection.upRight:
+      case JoystickDirection.downRight:
+        player.horizontalMovement = 1;
+        break;
+      default:
+        player.horizontalMovement = 0;
+    }
   }
 
   void _loadLevel() {
